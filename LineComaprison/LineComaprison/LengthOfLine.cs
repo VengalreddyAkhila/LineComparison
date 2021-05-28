74f202133c49c50b295f4fb1e6a14edd5fa6f0ab
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-05-28 13:01:30 +0530

    Refactor:LengthOfLine

---
+++ LineComaprison/LineComaprison/LengthOfLine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComaprison
+{
+    public class LengthOfLine
+    {
+
+        double lengthofline;
+        public void Lenght(int X1, int X2, int Y1, int Y2)
+        {
+            lengthofline  = Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2));
+            Console.WriteLine(lengthofline);
+        }
+    }
+}
---
+++ LineComaprison/LineComaprison/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LineComaprison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LengthOfLine lengthofline = new LengthOfLine();
+            lengthofline.Lenght(2, 10, 3, 20);
+        }
+    }
+}
