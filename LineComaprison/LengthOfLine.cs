cc954baf06b0ad4f7c85cca38e150bc73ad4a202
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-05-30 18:02:21 +0530

    Refactor:UC1LengthOfLine

---
+++ LineComaprison/LengthOfLine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComaprison
+{
+    public class LengthOfLine
+    {
+        public double  Length(double X1, double X2, double Y1, double Y2)
+        {
+            //double a;
+           // double b;
+            double lengthofline  = Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2));
+            Console.WriteLine(lengthofline);
+            return lengthofline;
+ 
+           
+        }
+    }
+}
--- LineComaprison/Program.cs
+++ LineComaprison/Program.cs
@@ -6,7 +6,19 @@ namespace LineComaprison
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to LineComparison");
+
+            LengthOfLine  line1 = new LengthOfLine();
+            LengthOfLine  line2 = new LengthOfLine ();
+            double a = line1.Length(3, 7, 10, 12);
+            double b = line2.Length(5, 7, 10, 12);
+            if (a == b)
+            {
+                Console.WriteLine("lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("lines are not equal");
+            }
         }
     }
 }
