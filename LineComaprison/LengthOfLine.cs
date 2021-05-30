8c0f3ddb53162310189319cbd25e9be5c27366df
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-05-30 18:05:18 +0530

    Refactor:UC1LengthOfLine

--- LineComaprison/LengthOfLine.cs
+++ LineComaprison/LengthOfLine.cs
@@ -7,14 +7,11 @@ namespace LineComaprison
     public class LengthOfLine
     {
         public double  Length(double X1, double X2, double Y1, double Y2)
-        {
-            //double a;
-           // double b;
+        { 
             double lengthofline  = Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2));
             Console.WriteLine(lengthofline);
             return lengthofline;
- 
-           
+         
         }
     }
 }
