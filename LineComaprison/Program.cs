745d8be1c151539d79f134fdd69cc8f72f3fd912
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-06-03 21:35:03 +0530

    Refactor:UC4-OOpsConcept

--- LineComaprison/Program.cs
+++ LineComaprison/Program.cs
@@ -7,27 +7,21 @@ namespace LineComaprison
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to LineComparision");
-            LengthOfLine length;
-            length = new LengthOfLine();
-            // Calculate cal;
-            //cal = new Calculate_length();
-            double Line1 = length.Line(1, 1, 1, 2);
-            double Line2 = length.Line(1, 2, 2, 2); ;
-            //Equals
-            if (Line1  == Line2 )
+            LengthOfLine  length = new LengthOfLine();
+            double Line1 = length.Line(1, 2, 3, 2);
+            double Line2 = length.Line(1, 2, 4, 6); 
 
+            //Lines Equals
+            if (Line1  == Line2 )
                 Console.WriteLine("lines are equal");
-            
             else
-            
-                Console.WriteLine("lines are not equal");
+               Console.WriteLine("lines are not equal");
                         
-            //compareTo
+            //compare lines
             if (Line1.CompareTo(Line2) == 0)
                 Console.WriteLine("Both Line are  equal");
             else
                 Console.WriteLine("They are not equal");
-           // Console.ReadKey();
         }
     }
 }
