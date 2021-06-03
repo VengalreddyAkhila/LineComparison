af2d17e593fbe3b86e94c88fa433d08656f8283c
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-06-03 21:49:54 +0530

    Refactor:UC1-LengthOfLine

--- LineComaprison/LengthOfLine.cs
+++ LineComaprison/LengthOfLine.cs
@@ -4,14 +4,25 @@ using System.Text;
 
 namespace LineComaprison
 {
-    public class LengthOfLine
+     
+    class LengthOfLine 
     {
-        public double  Length(double X1, double X2, double Y1, double Y2)
+        private double X1;
+        private double X2;
+        private double Y1;
+        private  double Y2;
+        public   LengthOfLine(double X1, double X2, double Y1, double Y2)
+        {
+            this.X1 = X1;
+            this.X2 = X2;
+            this.Y1 = Y1;
+            this.Y2 = Y2;
+        }
+        public double Length()
         { 
-            double lengthofline  = Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2));
+            double lengthofline = Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2));
             Console.WriteLine(lengthofline);
             return lengthofline;
-         
         }
     }
 }
--- LineComaprison/Program.cs
+++ LineComaprison/Program.cs
@@ -6,19 +6,10 @@ namespace LineComaprison
     {
         static void Main(string[] args)
         {
-
-            LengthOfLine  line1 = new LengthOfLine();
-            LengthOfLine  line2 = new LengthOfLine ();
-            double a = line1.Length(3, 7, 10, 12);
-            double b = line2.Length(5, 7, 10, 12);
-            if (a == b)
-            {
-                Console.WriteLine("lines are equal");
-            }
-            else
-            {
-                Console.WriteLine("lines are not equal");
-            }
+            Console.WriteLine("Welcome to LineComparision");
+            LengthOfLine  length = new LengthOfLine(4, 6, 8, 10);
+            Console.WriteLine(length);
+            
         }
     }
 }
